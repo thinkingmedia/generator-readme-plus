0633cdf523ff591aa8b1537f1045f065d7f60f0b
thinkingmedia <mathew@thinkingmedia.ca>
2015-12-10 15:34:39 -0500

    removed code related to node module to old folder for keeping

--- app/index.js
+++ app/index.js
@@ -4,18 +4,35 @@ var generators = require('yeoman-generator');
  * @class
  * @lends yo.YeomanGeneratorBase
  **/
-var readMe = {
-    /**
-     * @constructs
-     */
-    constructor: function () {
-        generators.Base.apply(this, arguments);
-        this.option('coffee', {});
+var readMePlus = {
+    initializing: function () {
+        this.props = {};
     },
 
-    method1: function () {
-        console.log('method 1 just run');
+    prompting: function () {
+        var done = this.async();
+
+        this.prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'Project Name',
+            default: 'Hello World'
+        },{
+            type: 'confirm',
+            name: 'toc',
+            message: 'Table Of Contents',
+            default: true
+        }], function (answers) {
+            this.log(answers);
+            done();
+        }.bind(this));
+    },
+
+    writing: function () {
+        //var readme = this.fs.read(this.destinationPath('README+.md'));
+        //console.log(readme);
+        //this.fs.writeJSON(this.destinationPath('README+.md'), readme);
     }
 };
 
-module.exports = generators.Base.extend(readMe);
+module.exports = generators.Base.extend(readMePlus);
